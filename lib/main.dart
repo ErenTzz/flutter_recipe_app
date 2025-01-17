75883cde9ec132b02ef8b865b483ed02e01514d0
Eren Toksöz <eren.toksoz@hotmail.com>
2025-01-17 11:40:20 +0300

    onboarding page update

--- lib/main.dart
+++ lib/main.dart
@@ -14,49 +14,137 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(), // Scaffold'u buraya taşıdık
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
         children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Image(
-              image: AssetImage("assets/images/Onboarding.png"),
-              width: 600,
-              height: 600,
+          Container(
+            padding: EdgeInsets.all(0.01),
+            margin:
+                const EdgeInsets.all(0.01), // Sağ, sol, üst ve alt boşluklar
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 100,
+                  left: 10,
+                  child: Image(
+                    image: AssetImage("assets/OnboardingImages/image 1.png"),
+                    width: 225,
+                    height: 225,
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  left: 130,
+                  child: Image(
+                    image: AssetImage("assets/OnboardingImages/image 3.png"),
+                    width: 325,
+                    height: 325,
+                  ),
+                ),
+                Positioned(
+                  top: 240,
+                  left: 120,
+                  child: Image(
+                    image: AssetImage("assets/OnboardingImages/image 4.png"),
+                    width: 225,
+                    height: 225,
+                  ),
+                ),
+                Positioned(
+                  top: 440,
+                  left: 200,
+                  child: Transform(
+                    transform: Matrix4.rotationZ(
+                        3.1), // X ekseninde 180 derece döndürme
+                    alignment: Alignment.center,
+                    child: Image(
+                      image: AssetImage(
+                          "assets/OnboardingImages/Rectangle 188.png"),
+                      width: 125,
+                      height: 125,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 225,
+                  left: 335,
+                  child: Image(
+                    image: AssetImage("assets/OnboardingImages/Ellipse 7.png"),
+                    width: 125,
+                    height: 125,
+                  ),
+                ),
+                Positioned(
+                  top: 250,
+                  left: -25,
+                  child: Image(
+                    image: AssetImage("assets/OnboardingImages/Ellipse 2.png"),
+                    width: 145,
+                    height: 145,
+                  ),
+                ),
+                Positioned(
+                  top: 350,
+                  left: 300,
+                  child: Image(
+                    image: AssetImage("assets/OnboardingImages/Ellipse 6.png"),
+                    width: 150,
+                    height: 150,
+                  ),
+                ),
+                Positioned(
+                  top: 400,
+                  left: 25,
+                  child: Image(
+                    image: AssetImage("assets/OnboardingImages/Ellipse 3.png"),
+                    width: 160,
+                    height: 160,
+                  ),
+                ),
+              ],
             ),
           ),
-          const SizedBox(height: 50),
-          Text(
-            "Start Cooking",
-            style: context.h1.copyWith(
-              color: AppThemeColor.mainText,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            "Let’s join our community to cook better food!",
-            style: context.p1.copyWith(color: AppThemeColor.secondaryText),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 20),
-          PrimaryButton(
-            text: "Get Started",
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => const SignInPage()),
-              );
-            },
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 650,
+                width: 600,
+              ),
+              Text(
+                "Start Cooking",
+                style: context.h1.copyWith(
+                  color: AppThemeColor.mainText,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Let’s join our community to cook better food!",
+                style: context.p1.copyWith(color: AppThemeColor.secondaryText),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              PrimaryButton(
+                text: "Get Started",
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const SignInPage()),
+                  );
+                },
+              ),
+            ],
           ),
         ],
       ),
